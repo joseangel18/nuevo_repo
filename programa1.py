a43a77a72863e9d6f11b2acd6798538dbe870ae3
Jose Angel <joseangel.aosuna@gmail.com>
2023-02-13 10:42:38 -0700

    buscar por titulo de la pagina

--- programa1.py
+++ programa1.py
@@ -12,7 +12,7 @@ def gettitle(url):
         return None
     try:
         soup = BeautifulSoup(html.text, 'html.parser')
-        title = soup.find_all('h3', limit=6)
+        title = soup.find('title')
     except AttributeError as e:
         return None
 
@@ -20,12 +20,6 @@ def gettitle(url):
 
 
 title = gettitle("https://www.bbc.com/news/technology")
-print("BBC News Tecnología")
-for titulo in title:
-    print(titulo.text.strip())
-
-title = gettitle(
-    "https://www.marca.com/futbol/primera-division/clasificacion.html?intcmp=MENUMIGA&s_kw=clasificacion")
 print("BBC News Tecnología")
 for titulo in title:
     print(titulo.text.strip())
